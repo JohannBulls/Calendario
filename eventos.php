8c29f81288d6f4bcad2456f1614445e237c3e881
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2018-12-13 13:45:45 -0500

    Eliminar

--- eventos.php
+++ eventos.php
@@ -6,33 +6,59 @@ $accion = (isset($_GET['accion'])) ? $_GET['accion'] : 'leer';
 switch ($accion) {
     case 'agregar':
         // Intruccion de agregado
-        $sentenciaSQL = $pdo->prepare("INSERT INTO eventos(title,descripcion,color,textColor,start,end) VALUES (:title,:descripcion,:color,:textColor,:start,:end)");
+        $sentenciaSQL = $pdo->prepare("INSERT INTO eventos(
+            title,
+            descripcion,
+            color,
+            textColor,
+            start,
+            end)
+            VALUES (:title,:descripcion,:color,:textColor,:start,:end)");
 
-        $respuesta=$sentenciaSQL->execute(array(
-                                                "title" => $_POST['title'],
-                                                "descripcion" => $_POST['descripcion'],
-                                                "color" => $_POST['color'],
-                                                "textColor" => $_POST['txtColor'],
-                                                "start" => $_POST['start'],
-                                                "end" => $_POST['end'],
+        $respuesta = $sentenciaSQL->execute(array(
+            "title" => $_POST['title'],
+            "descripcion" => $_POST['descripcion'],
+            "color" => $_POST['color'],
+            "textColor" => $_POST['txtColor'],
+            "start" => $_POST['start'],
+            "end" => $_POST['end'],
         ));
         echo json_encode($respuesta);
         break;
     case 'eliminar':
-    // Intruccion de eliminar
-        $respuesta=false;
-        if(isset($_POST['id'])){
-            $sentenciaSQL= $pdo->prepare("DELETE FROM eventos WHERE id=:id");
-            $respuesta= $sentenciaSQL->execute(array("id"=>$_POST['id']));
+        // Intruccion de eliminar
+        $respuesta = false;
+        if (isset($_POST['id'])) {
+            $sentenciaSQL = $pdo->prepare("DELETE FROM eventos WHERE id=:id");
+            $respuesta = $sentenciaSQL->execute(array("id" => $_POST['id']));
         }
         echo json_encode($respuesta);
-        
         break;
     case 'modificar':
-        echo "Instruccion modificar";
+        // Intruccion de modificar
+        $sentenciaSQL = $pdo->prepare("UPDATE eventos SET
+            title=:title,
+            descripcion =:descripcion,
+            color =:color,
+            textColor =:textColor,
+            start =:start,
+            end =:end,
+            WHERE id=:id
+        ");
+
+        $respuesta = $sentenciaSQL->execute(array(
+            "id" =>$_POST['id'],
+            "title" => $_POST['title'],
+            "descripcion" => $_POST['descripcion'],
+            "color" => $_POST['color'],
+            "textColor" => $_POST['txtColor'],
+            "start" => $_POST['start'],
+            "end" => $_POST['end'],
+        ));
+        echo json_encode($respuesta);
         break;
     default:
-    //Seleccionar los eventos del calendario
+        //Seleccionar los eventos del calendario
         $sentenciaSQL = $pdo->prepare("SELECT * FROM eventos");
         $sentenciaSQL->execute();
 
